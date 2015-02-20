6a8b15066ac7439dcbd5be11d44fb66cf9bfce08
Sebastian Bensusan <sbensu@gmail.com>
2015-02-20 22:53:00 +0100

    Init commit

---
+++ dev_src/routing/dev.cljs
@@ -0,0 +1,10 @@
+(ns routing.dev
+    (:require
+     [routing.core]
+     [figwheel.client :as fw]))
+
+(fw/start {
+  :websocket-url "ws://localhost:3449/figwheel-ws"
+  :on-jsload (fn []
+               ;; (stop-and-start-my app)
+               )})
