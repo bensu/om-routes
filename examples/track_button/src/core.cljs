1453b1aa170b34e1de485c84b9c14d2cc48a9aae
Sebastian Bensusan <sbensu@gmail.com>
2015-03-23 23:02:51 +0100

    Added new folder structure for examples

---
+++ examples/track_button/src/core.cljs
@@ -0,0 +1,60 @@
+(ns routing.example
+    (:require-macros [cljs.core.async.macros :refer [go]])
+    (:require [om.core :as om :include-macros true]
+              [cljs.core.async :as async :refer [put! chan]]
+              [routing.core :as routing]
+              [om.dom :as dom :include-macros true]))
+
+(enable-console-print!)
+
+;; Main State
+
+(defonce app-state (atom {:nav {:method :button}}))
+
+;; Navigation API
+
+(def nav-path :nav)
+
+(defn get-nav [data]
+  (get-in data [nav-path :last-click]))
+
+(defn nav-to [view-cursor method]
+  (om/update! view-cursor [nav-path :last-click] method :routing.core/nav))
+
+(defn url->state [{:keys [last-click]}]
+  {:last-click (keyword last-click)})
+
+(def route [["#" :last-click] (routing/make-handler url->state)])
+
+;; View
+
+(defn view-component [data owner]
+  (om/component
+   (dom/div nil
+            (dom/h1 nil (case (get-nav data)
+                          :button "A button got me here"
+                          :link "A link got me here"
+                          "Who got me here?"))
+            (dom/button #js {:onClick (fn [_] (nav-to data :button))} "Button") 
+            (dom/br nil)
+            (dom/a #js {:href "#link"} "Link"))))
+
+;; Main Component
+
+(let [tx-chan (chan)
+      tx-pub-chan
+      (async/pub tx-chan (fn [_] :txs))]
+  (om/root
+   (fn [data owner]
+     (reify
+       om/IRender
+       (render [_]
+         (om/build routing/om-routes data
+                   {:opts {:view-component view-component
+                           :route route
+                           :nav-path nav-path}}))))
+   app-state
+   {:target (. js/document (getElementById "app"))
+    :shared {:tx-chan tx-pub-chan}
+    :tx-listen (fn [tx-data root-cursor]
+                 (put! tx-chan [tx-data root-cursor]))}))
